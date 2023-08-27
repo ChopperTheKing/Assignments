151bb4e60724d875b7ebfcdc7d36ced74e4db8ea
ChopperTheKing <rkissos@me.com>
2023-08-27 17:10:52 -0400

    created a robot

---
+++ Assignment3/Mars/Mars/main2.swift
@@ -0,0 +1,8 @@
+//
+//  main2.swift
+//  Mars
+//
+//  Created by Ronnie Kissos on 8/27/23.
+//
+
+import Foundation
---
+++ Assignment3/Mars2/Mars2/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Mars2
+//
+//  Created by Ronnie Kissos on 8/27/23.
+//
+
+import Foundation
+
+struct Robot {
+    var x: Int
+    var y: Int
+    var orientation: Character
+}
+
