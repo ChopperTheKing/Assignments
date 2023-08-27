94d0a41dc596ec3aeb009b2e3b6449bc03c25c30
ChopperTheKing <rkissos@me.com>
2023-08-26 20:25:56 -0400

    work on grid

--- Assignment3/Mars/Mars/ViewController.swift
+++ Assignment3/Mars/Mars/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
 
 }
 
+extension ViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: <#T##IndexPath#>, animated: true)
+        
+        print("You tapped me")
+    }
+}
+extension ViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 12
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: <#T##String#>, for: <#T##IndexPath#>)
+        
+        return cell
+    }
+    
+}
+extension ViewController: UICollectionViewDelegateFlowLayout {}
