c421a88fb0f17aff0a63e24ca4dfc78ca77425b7
ChopperTheKing <rkissos@me.com>
2023-08-27 20:16:53 -0400

    added robot class

--- Assignment3/Mars2/Mars2/Robot.swift
+++ Assignment3/Mars2/Mars2/Robot.swift
@@ -11,3 +11,68 @@ enum Orientation: Character {
     case north = "N", east = "E", south = "S", west = "W"
 }
 
+class Robot {
+    var x: Int
+    var y: Int
+    var orientation: Orientation
+    
+    init(x: Int, y: Int, orientation: Orientation) {
+        self.x = x
+        self.y = y
+        self.orientation = orientation
+    }
+    
+    func moveForward() {
+        switch orientation {
+        case .north:
+            y += 1
+        case .east:
+            x += 1
+        case .south:
+            y -= 1
+        case .west:
+            x -= 1
+        }
+    }
+    
+    func turnRight() {
+        switch orientation {
+        case .north:
+            orientation = .east
+        case .east:
+            orientation = .south
+        case .south:
+            orientation = .west
+        case .west:
+            orientation = .north
+        }
+    }
+    
+    func turnLeft() {
+        switch orientation {
+        case .north:
+            orientation = .west
+        case .west:
+            orientation = .south
+        case .south:
+            orientation = .east
+        case .east:
+            orientation = .north
+        }
+    }
+    
+    func followInstructions(_ instructions: String) {
+        for instruction in instructions {
+            switch instruction {
+            case "F":
+                moveForward()
+            case "R":
+                turnRight()
+            case "L":
+                turnLeft()
+            default:
+                print("Invalid instruction: \(instruction)")
+            }
+        }
+    }
+}
