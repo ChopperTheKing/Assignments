da69ca66a3f11e63ef4ad62c7017fe1b786be56a
ChopperTheKing <rkissos@me.com>
2023-08-31 07:21:52 -0400

    Finished with assignment

---
+++ Assignment6/Assignment6b/Assignment6b/main.swift
@@ -0,0 +1,39 @@
+//
+//  main.swift
+//  Assignment6b
+//
+//  Created by Ronnie Kissos on 8/31/23.
+//
+
+import Foundation
+
+enum ObjectCreationError: Error {
+    case maxObjectsReached
+}
+
+class LimitedObject {
+    static var objectCount = 0
+    
+    init() throws {
+        if LimitedObject.objectCount >= 3 {
+            throw ObjectCreationError.maxObjectsReached
+        }
+        
+        LimitedObject.objectCount += 1
+    }
+}
+
+do {
+    let obj1 = try LimitedObject()
+    let obj2 = try LimitedObject()
+    let obj3 = try LimitedObject()
+    
+    print("Three objects created successfully")
+    
+    let obj4 = try LimitedObject()
+    print("A fourth object was created")
+} catch ObjectCreationError.maxObjectsReached {
+    print("Error: Maximum number of objects reached")
+} catch {
+    print("An unknown error occurred")
+}
