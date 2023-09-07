c8d1d5074f0faa189f8107ab8f5c70dd47264f43
ChopperTheKing <rkissos@me.com>
2023-09-07 07:33:29 -0400

    utilize swift return type

---
+++ Assignment10/Assignments.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+
+// 
+
+
+//1. In Countires code, separate the netowork layer to different class.
+//
+//1.1 communicate from network Manager to Controller using Protocol Delegate
+//
+//1.1 communicate from network Manager to Controller using Escaping closure
+
+// In both 1.1 and 1.2 along with Countireis if api fails pass error back to view and show error in UI.
+
+
+
+
+// what is diff b/w escaping and non escaping
+
+// Closures can be either escaping or non-escaping. Non-escaping closures are closures that are passed as arguments to a function and are called within the same function. Escaping closures are closures that are passed as arguments to a function but are called after the function has completed its execution.
+
+// what is associated values
+
+// Associated values in Swift allow you to store additional custom information along with enum cases. This enables them to hold different types of values associated with their enum cases.
+
+// what is raw values
+
+// Raw values are simple, prepopulated values that you can assign to enum cases. Raw values can be strings, characters, or any of the integer or floating-point number types.
+
+// what is Generics
+
+// Generics in Swift (and many other programming languages) allow you to write flexible, reusable functions and types that can work with different data types while still preserving type safety. It's usually represeented by the letter T, but can be any letter.
+
---
+++ Assignment10/Countries/Countries/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by MAC on 05/09/23.
+//
+
+// ViewController.swift
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, NetworkManagerDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var countries: [Country] = [] //holds list of countries that'll be displayed in table view.
+    private let networkManager = NetworkManager() //instance of NetworkManager to make API call.
+    
+    override func viewDidLoad() { //Sets up initial state.
+        super.viewDidLoad()
+        networkManager.delegate = self
+        
+        tableView.dataSource = self //Sets data source of table view to self.
+        tableView.dataSource = self
+        networkManager.getCountries { countries, error in //Calls getCountries method to fetch countries.
+            if let countries = countries { //Checks if countries were fetched successfully and reloads table view.
+                self.countries = countries
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } else if let error = error { //Checks if error occurred and calls showError.
+                DispatchQueue.main.async {
+                    self.showError(error: error)
+                }
+            }
+        }
+    }
+    
+    func showError(error: Error) { //Shows alert if error occurs.
+        let alert = UIAlertController(title: "Error", message: error.localizedDescription, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { //Sets up table view.
+        return countries.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { //Sets up TV
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        
+        let country = countries[indexPath.row]
+        
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16.0)
+        
+        cell.textLabel?.text = country.name
+        cell.detailTextLabel?.text = "\(country.capital)"
+        
+        let rightLabel = UILabel()
+        
+        rightLabel.text = "\(country.currency.code), \(country.code)"
+        
+        rightLabel.font = UIFont.systemFont(ofSize: 12)
+        rightLabel.sizeToFit()
+        
+        cell.accessoryView = rightLabel
+
+        return cell
+    }
+    
+    func didFetchCountries(_ countries: [Country]) { //Make ViewController conform to NetworkManagerDelegate protocol
+        self.countries = countries
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+
+    func didFailWithError(_ error: Error) {
+        DispatchQueue.main.async {
+            self.showError(error: error)
+        }
+    }
+    
+}
